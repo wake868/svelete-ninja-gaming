20fe24bb54a35ae8bc3970b011276214e63d8865
Mike Wakeland <wake868@gmail.com>
2022-03-12 21:10:55 -0800

    underline on the back link

--- src/routes/guides/[id].svelte
+++ src/routes/guides/[id].svelte
@@ -43,6 +43,6 @@
         d="M11 15l-3-3m0 0l3-3m-3 3h8M3 12a9 9 0 1118 0 9 9 0 01-18 0z"
       />
     </svg>
-    <a href="/guides">BACK</a>
+    <a class="underline" href="/guides">BACK</a>
   </div>
 </div>
